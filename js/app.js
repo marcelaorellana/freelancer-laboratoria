54804af51bf0d1810adfb60cf7665696f3816b8c
marcelaOrellana <marcelapaz.1985@gmail.com>
2017-06-01 10:11:38 -0400

    Mas arreglos de estilos

--- js/app.js
+++ js/app.js
@@ -9,3 +9,31 @@ window.addEventListener("scroll", function(){
     navi.classList.remove('shrink');
   }
 });
+
+(function main(){
+	var boxes = Array.from(document.getElementsByClassName("col-4"));
+	var modal = document.getElementById("modalId");
+	var bodyModal, close, img;
+
+	boxes.forEach(function(box){
+		box.addEventListener("click", function(){
+			
+			modal.innerHTML = "";
+
+			modal.innerHTML = box.innerHTML;			
+			
+			modal.classList.remove("hide");
+
+			close = document.createElement("div");
+			close.classList.add("close");
+			img = document.createElement("img");
+			close.appendChild(img);
+			modal.appendChild(close);
+			img.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
+			
+			close.addEventListener("click",function(){
+				modal.classList.add("hide");
+			});
+		});		
+	});
+})();
