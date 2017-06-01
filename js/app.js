2f95c367427fe8c632152da54254b56862fdbcc0
marcelaorellana <marcelapaz.1985@gmail.com>
2017-06-01 00:37:23 -0400

    funcion del scroll, falta que funcione

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
-/*
- * Archivo principal de JS
- */
+
+var navi = document.getElementById("navegadorId");
+
+window.addEventListener("scroll", function(){
+	
+	if (document.body.scrollTop > 150) {
+    navi.classList.add('shrink');
+  } else {
+    navi.classList.remove('shrink');
+  }
+});
